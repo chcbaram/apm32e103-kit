bb8bebbbf94516d8bb3d6d4f3faf3e37a3ba51b0
Hancheol Cho <chcbaram@gmail.com>
2024-04-16 12:35:25 +0900

    CLI 예제 추가

---
+++ firmware/exam/EX-03-CLI/src/bsp/bsp.h
@@ -0,0 +1,31 @@
+#ifndef BSP_H_
+#define BSP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "def.h"
+
+
+#include "apm32e10x.h"
+#include "apm32e10x_gpio.h"
+#include "apm32e10x_rcm.h"
+#include "apm32e10x_usart.h"
+#include "apm32e10x_dma.h"
+
+
+void logPrintf(const char *fmt, ...);
+
+
+bool bspInit(void);
+
+void delay(uint32_t time_ms);
+uint32_t millis(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
