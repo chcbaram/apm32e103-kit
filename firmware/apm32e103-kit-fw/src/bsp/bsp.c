e80c84387a804baffafa0cbec4e0bf31e986c7a5
Hancheol Cho <chcbaram@gmail.com>
2024-04-16 09:44:57 +0900

    초기 프로젝트 추가

---
+++ firmware/apm32e103-kit-fw/src/bsp/bsp.c
@@ -0,0 +1,53 @@
+#include "bsp.h"
+#include "hw_def.h"
+
+
+volatile static uint32_t systick_ms = 0;
+
+
+
+void SysTick_Handler(void)
+{
+  systick_ms++;
+}
+
+
+
+
+bool bspInit(void)
+{
+
+  SysTick_Config(SystemCoreClock / 1000U);
+
+  return true;
+}
+
+void delay(uint32_t time_ms)
+{
+  uint32_t pre_time = systick_ms;
+
+  while(systick_ms-pre_time < time_ms);
+}
+
+uint32_t millis(void)
+{
+  return systick_ms;
+}
+
+
+
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  char *name_buf;
+
+  if (strrchr((char *) file,'/') == NULL) 
+  {
+    name_buf = strrchr((char *)file,'\\')+1;
+  }
+  else 
+  {
+    name_buf = strrchr((char *)file,'/')+1;
+  }
+
+  printf("assert_faile() - %s : %d", name_buf, (int)line);
+}
