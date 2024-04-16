86ecb8a4f1f2739e9306973f957831ffdeb60a43
Hancheol Cho <chcbaram@gmail.com>
2024-04-16 09:52:10 +0900

    LED 예제 추가

---
+++ firmware/exam/EX-01-LED/src/hw/hw_def.h
@@ -0,0 +1,23 @@
+#ifndef HW_DEF_H_
+#define HW_DEF_H_
+
+
+
+#include "bsp.h"
+
+
+#define _DEF_FIRMWATRE_VERSION    "V240416R1"
+#define _DEF_BOARD_NAME           "APM32E103-KIT-FW"
+
+
+
+#define _USE_HW_LED
+#define      HW_LED_MAX_CH          1
+
+
+//-- USE CLI
+//
+#define _USE_CLI_HW_LED             1
+
+
+#endif
