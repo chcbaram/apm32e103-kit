9b0f7d35b1b327c78bf1bcc4e217e6ed5ac13a2f
Hancheol Cho <chcbaram@gmail.com>
2024-04-28 21:01:56 +0900

    온습도 예제 추가

---
+++ firmware/exam/EX-17-HDC1080/src/hw/driver/rtc.c
@@ -0,0 +1,280 @@
+#include "rtc.h"
+
+
+
+#ifdef _USE_HW_RTC
+#include "cli.h"
+#include <time.h>
+
+
+#if CLI_USE(HW_RTC)
+static void cliRtc(cli_args_t *args);
+#endif
+
+static bool is_init = false;
+
+
+
+
+
+bool rtcInit(void)
+{
+  bool ret = false;
+
+
+  RCM_EnableAPB1PeriphClock((RCM_APB1_PERIPH_T)(RCM_APB1_PERIPH_PMU | RCM_APB1_PERIPH_BAKR));
+  PMU_EnableBackupAccess();
+ 
+  RCM_ConfigLSE(RCM_LSE_OPEN);
+  delay(10);
+
+  RCM_ConfigRTCCLK(RCM_RTCCLK_LSE);
+  RCM_EnableRTCCLK();
+
+  RTC_WaitForSynchro();
+  RTC_ConfigPrescaler(32767);
+  RTC_WaitForLastTask();
+
+  delay(10);
+  ret = RCM_ReadStatusFlag(RCM_FLAG_LSERDY);
+
+  logPrintf("[%s] rtcInit()\n", ret ? "OK":"NG");
+  is_init = ret;
+
+#if CLI_USE(HW_RTC)
+  cliAdd("rtc", cliRtc);
+#endif
+  return ret;
+}
+
+bool rtcIsInit(void)
+{
+  return is_init;
+}
+
+bool rtcGetInfo(rtc_info_t *rtc_info)
+{
+  time_t    cur_time;
+  struct tm timeinfo;
+
+  cur_time = RTC_ReadCounter();
+
+  gmtime_r(&cur_time, &timeinfo);
+
+  rtc_info->time.hours   = timeinfo.tm_hour;
+  rtc_info->time.minutes = timeinfo.tm_min;
+  rtc_info->time.seconds = timeinfo.tm_sec;
+
+  rtc_info->date.year  = (timeinfo.tm_year + 1900) % 100;
+  rtc_info->date.month = timeinfo.tm_mon + 1;
+  rtc_info->date.day   = timeinfo.tm_mday;
+  rtc_info->date.week  = timeinfo.tm_wday;
+
+  return true;
+}
+
+bool rtcGetTime(rtc_time_t *rtc_time)
+{
+  rtc_info_t rtc_info;
+
+
+  rtcGetInfo(&rtc_info);
+
+
+  rtc_time->hours   = rtc_info.time.hours;
+  rtc_time->minutes = rtc_info.time.minutes;
+  rtc_time->seconds = rtc_info.time.seconds;
+
+  return true;
+}
+
+bool rtcGetDate(rtc_date_t *rtc_date)
+{
+  rtc_info_t rtc_info;
+
+
+  rtcGetInfo(&rtc_info);
+
+  rtc_date->year  = rtc_info.date.year;
+  rtc_date->month = rtc_info.date.month;
+  rtc_date->day   = rtc_info.date.day;
+  rtc_date->week  = rtc_info.date.week;
+
+  return true;
+}
+
+bool rtcSetTime(rtc_time_t *rtc_time)
+{
+  rtc_info_t rtc_info;
+
+
+  rtcGetInfo(&rtc_info);
+
+  rtc_info.time.hours   = rtc_time->hours;
+  rtc_info.time.minutes = rtc_time->minutes;
+  rtc_info.time.seconds = rtc_time->seconds;
+
+  struct tm timeinfo;
+  time_t    cur_time;
+
+  memset(&timeinfo, 0, sizeof(timeinfo));
+  timeinfo.tm_year  = (rtc_info.date.year + 2000) - 1900;
+  timeinfo.tm_mon   = rtc_info.date.month - 1;
+  timeinfo.tm_mday  = rtc_info.date.day;
+  timeinfo.tm_hour  = rtc_info.time.hours;
+  timeinfo.tm_min   = rtc_info.time.minutes;
+  timeinfo.tm_sec   = rtc_info.time.seconds;
+
+  cur_time = mktime(&timeinfo);
+
+  RTC_ConfigCounter((uint32_t)cur_time);
+  delay(10);
+
+  return true;
+}
+
+bool rtcSetDate(rtc_date_t *rtc_date)
+{
+  rtc_info_t rtc_info;
+
+
+  rtcGetInfo(&rtc_info);
+
+  rtc_info.date.year  = rtc_date->year;
+  rtc_info.date.month = rtc_date->month;
+  rtc_info.date.day   = rtc_date->day;
+
+
+  struct tm timeinfo;
+  time_t    cur_time;
+
+  memset(&timeinfo, 0, sizeof(timeinfo));
+  timeinfo.tm_year  = (rtc_info.date.year + 2000) - 1900;
+  timeinfo.tm_mon   = rtc_info.date.month - 1;
+  timeinfo.tm_mday  = rtc_info.date.day;
+  timeinfo.tm_hour  = rtc_info.time.hours;
+  timeinfo.tm_min   = rtc_info.time.minutes;
+  timeinfo.tm_sec   = rtc_info.time.seconds;
+
+  cur_time = mktime(&timeinfo);
+
+  RTC_ConfigCounter((uint32_t)cur_time);
+  delay(10);
+
+  return true;
+}
+
+
+bool rtcSetReg(uint32_t index, uint32_t data)
+{
+  #if 0
+  if (IS_RTC_BKP(index))
+  {
+    HAL_RTCEx_BKUPWrite(&hrtc, index, data);
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+  #else
+  return false;
+  #endif
+}
+
+bool rtcGetReg(uint32_t index, uint32_t *p_data)
+{
+  #if 0
+  if (IS_RTC_BKP(index))
+  {
+    *p_data = HAL_RTCEx_BKUPRead(&hrtc, index);
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+  #else
+  return false;
+  #endif
+}
+
+
+#if CLI_USE(HW_RTC)
+void cliRtc(cli_args_t *args)
+{
+  bool ret = false;
+
+
+  if (args->argc == 1 && args->isStr(0, "info"))
+  {
+    cliPrintf("is_init : %d\n", is_init);
+    ret = true;
+  }
+
+  if (args->argc == 2 && args->isStr(0, "get") && args->isStr(1, "info"))
+  {
+    rtc_info_t rtc_info;
+    const char *week_str[] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+
+    while(cliKeepLoop())
+    {
+      rtcGetInfo(&rtc_info);
+
+      cliPrintf("Y:%02d M:%02d D:%02d, H:%02d M:%02d S:%02d %s\n",
+                rtc_info.date.year,
+                rtc_info.date.month,
+                rtc_info.date.day,
+                rtc_info.time.hours,
+                rtc_info.time.minutes,
+                rtc_info.time.seconds,
+                week_str[rtc_info.date.week]);
+      delay(1000);
+    }
+    ret = true;
+  }
+
+  if (args->argc == 5 && args->isStr(0, "set") && args->isStr(1, "time"))
+  {
+    rtc_time_t rtc_time;
+
+    rtc_time.hours = args->getData(2);
+    rtc_time.minutes = args->getData(3);
+    rtc_time.seconds = args->getData(4);
+
+    rtcSetTime(&rtc_time);
+    cliPrintf("H:%02d M:%02d S:%02d\n",
+              rtc_time.hours,
+              rtc_time.minutes,
+              rtc_time.seconds);
+    ret = true;
+  }
+
+  if (args->argc == 5 && args->isStr(0, "set") && args->isStr(1, "date"))
+  {
+    rtc_date_t rtc_date;
+
+    rtc_date.year = args->getData(2);
+    rtc_date.month = args->getData(3);
+    rtc_date.day = args->getData(4);
+
+    rtcSetDate(&rtc_date);
+    cliPrintf("Y:%02d M:%02d D:%02d\n",
+              rtc_date.year,
+              rtc_date.month,
+              rtc_date.day);
+    ret = true;
+  }
+
+
+  if (ret == false)
+  {
+    cliPrintf("rtc info\n");
+    cliPrintf("rtc get info\n");
+    cliPrintf("rtc set time [h] [m] [s]\n");
+    cliPrintf("rtc set date [y] [m] [d]\n");
+  }
+}
+#endif
+
+#endif
---
+++ firmware/exam/EX-17-HDC1080/src/hw/hw_def.h
@@ -0,0 +1,134 @@
+#ifndef HW_DEF_H_
+#define HW_DEF_H_
+
+
+
+#include "bsp.h"
+
+
+#define _DEF_FIRMWATRE_VERSION    "V240416R1"
+#define _DEF_BOARD_NAME           "APM32E103-KIT-FW"
+
+
+#define _USE_HW_GPIO
+#define _USE_HW_RTC
+#define _USE_HW_SD
+#define _USE_HW_FATFS
+#define _USE_HW_FILES
+#define _USE_HW_BUZZER
+#define _USE_HW_ICM42670
+#define _USE_HW_IMU
+#define _USE_HW_HDC1080 
+
+
+#define _USE_HW_LED
+#define      HW_LED_MAX_CH          1
+
+#define _USE_HW_UART
+#define      HW_UART_MAX_CH         3
+#define      HW_UART_CH_SWD         _DEF_UART1
+#define      HW_UART_CH_CLI         _DEF_UART1
+#define      HW_UART_CH_USB         _DEF_UART2
+#define      HW_UART_CH_EXT         _DEF_UART3
+
+#define _USE_HW_LOG
+#define      HW_LOG_CH              _DEF_UART1
+#define      HW_LOG_BOOT_BUF_MAX    2048
+#define      HW_LOG_LIST_BUF_MAX    4096
+
+#define _USE_HW_CLI
+#define      HW_CLI_CMD_LIST_MAX    32
+#define      HW_CLI_CMD_NAME_MAX    16
+#define      HW_CLI_LINE_HIS_MAX    8
+#define      HW_CLI_LINE_BUF_MAX    64
+
+#define _USE_HW_BUTTON
+#define      HW_BUTTON_MAX_CH       1
+
+#define _USE_HW_I2C
+#define      HW_I2C_MAX_CH          2
+#define      HW_I2C_CH_EEPROM       _DEF_I2C1
+#define      HW_I2C_CH_OLED         _DEF_I2C2
+#define      HW_I2C_CH_IMU          _DEF_I2C1
+
+#define _USE_HW_EEPROM
+#define      HW_EEPROM_MAX_SIZE     (8*1024)
+
+#define _USE_HW_SPI
+#define      HW_SPI_MAX_CH          2
+
+#define _USE_HW_SPI_FLASH
+#define      HW_SPI_FLASH_ADDR      0x91000000
+
+#define _USE_HW_I2S
+#define      HW_I2S_LCD             0
+
+#define _USE_HW_MIXER
+#define      HW_MIXER_MAX_CH        4
+#define      HW_MIXER_MAX_BUF_LEN   (48*2*4*4) // 48Khz * Stereo * 4ms * 4
+
+#define _USE_HW_USB
+#define _USE_HW_CDC
+#define      HW_USE_CDC             1
+
+#define _USE_HW_CAN
+#define      HW_CAN_FD              0
+#define      HW_CAN_MAX_CH          1
+#define      HW_CAN_MSG_RX_BUF_MAX  32
+
+#define _USE_HW_WS2812
+#define     HW_WS2812_MAX_CH        1
+
+#define _USE_HW_LCD
+#define _USE_HW_SSD1306
+#define      HW_LCD_WIDTH           128
+#define      HW_LCD_HEIGHT          32
+
+#define _USE_HW_SWTIMER
+#define      HW_SWTIMER_MAX_CH      8
+
+#define _USE_HW_WIZNET
+#define      HW_WIZNET_SOCKET_CMD   0
+#define      HW_WIZNET_SOCKET_DHCP  1
+#define      HW_WIZNET_SOCKET_SNTP  2
+
+#define _USE_HW_EVENT
+#define      HW_EVENT_Q_MAX         8
+#define      HW_EVENT_NODE_MAX      16  
+
+
+
+//-- USE CLI
+//
+#define _USE_CLI_HW_LED             1
+#define _USE_CLI_HW_BUTTON          1
+#define _USE_CLI_HW_GPIO            1
+#define _USE_CLI_HW_I2C             1
+#define _USE_CLI_HW_EEPROM          1
+#define _USE_CLI_HW_SPI_FLASH       1
+#define _USE_CLI_HW_RTC             1
+#define _USE_CLI_HW_SD              1
+#define _USE_CLI_HW_FATFS           1
+#define _USE_CLI_HW_I2S             1
+#define _USE_CLI_HW_USB             1
+#define _USE_CLI_HW_CAN             1
+#define _USE_CLI_HW_WS2812          1
+#define _USE_CLI_HW_EVENT           1
+#define _USE_CLI_HW_WIZNET          1
+#define _USE_CLI_HW_ICM42670        1
+#define _USE_CLI_HW_IMU             1
+#define _USE_CLI_HW_HDC1080         1
+
+
+typedef enum
+{
+  SD_DETECT,
+  SPI_CS,
+  I2S_MUTE,
+  SPI2_CS,
+  W5500_RST,
+  W5500_INT,
+  GPIO_PIN_MAX,  
+} GpioPinName_t;
+
+#endif
